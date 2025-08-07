2c6c17b54626563f42f417b26f2dbaa47a34bdf6
RAVOKERcodes <shridhardhruv123@gmail.com>
2025-08-07 22:44:25 +0530

    FEAT: base url

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/orbit-club-connect">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
