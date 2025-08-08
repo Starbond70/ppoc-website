18c4385ed7801b733ce8bc9dd4c94b7b103eb349
starbond70@gmail.com <your@email.com>
2025-08-08 09:13:57 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/orbit-club-connect">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
